a93c2d6179eccccf13b777edb0dd88be520f03be
spikedcannonball <145741597+spikedcannonball@users.noreply.github.com>
2023-09-22 09:53:29 -0400

    Update FgcMode.cpp

--- src/modes/FgcMode.cpp
+++ src/modes/FgcMode.cpp
@@ -1,16 +1,20 @@
 #include "modes/FgcMode.hpp"
 
 FgcMode::FgcMode(socd::SocdType horizontal_socd, socd::SocdType vertical_socd) {
-    _socd_pair_count = 4;
+    _socd_pair_count = 2;
     _socd_pairs = new socd::SocdPair[_socd_pair_count]{
         socd::SocdPair{&InputState::left,   &InputState::right, horizontal_socd         },
  /* Mod X override C-Up input if both are pressed. Without this, neutral SOCD doesn't work
   properly if Down and both Up buttons are pressed, because it first resolves Down + Mod X
   to set both as unpressed, and then it sees C-Up as pressed but not Down, so you get an up
-  input instead of neutral. */
-        socd::SocdPair{ &InputState::mod_x, &InputState::c_up,  socd::SOCD_DIR1_PRIORITY},
+  input instead of neutral. 
+        Uncomment line 14 if we use c_up as up and comment out line 13
+  */
         socd::SocdPair{ &InputState::down,  &InputState::mod_x, vertical_socd           },
-        socd::SocdPair{ &InputState::down,  &InputState::c_up,  vertical_socd           },
+        //socd::SocdPair{ &InputState::down,  &InputState::c_up,  vertical_socd           },
+
+        //If using WASD, uncomment the below line and comment the above lines
+        //socd::SocdPair{ &InputState::down,   &InputState::w, vertical_socd     },
     };
 }
 
@@ -19,18 +23,25 @@ void FgcMode::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
     outputs.dpadLeft = inputs.left;
     outputs.dpadRight = inputs.right;
     outputs.dpadDown = inputs.down;
-    outputs.dpadUp = inputs.mod_x || inputs.c_up;
+    outputs.dpadUp = inputs.mod_x
 
     // Menu keys
-    outputs.start = inputs.start;
-    outputs.select = inputs.c_left;
-    outputs.home = inputs.c_down;
+    outputs.select = inputs.a;
+    //function layer
+    if (inputs.mod_y){
+        outputs.home = inputs.start;
+    } else {
+        outputs.start = inputs.start;
+    }
+    outputs.leftStickClick = inputs.c_left;
+    outputs.rightStickClick = inputs.c_up;
 
     // Right hand bottom row
     outputs.a = inputs.b;
     outputs.b = inputs.x;
     outputs.triggerRDigital = inputs.z;
-    outputs.triggerLDigital = inputs.up;
+    // remapped this to add left hand pinky button
+    outputs.triggerLDigital = inputs.l;
 
     // Right hand top row
     outputs.x = inputs.r;
